708f2e66f00be99ce48a6aa47f90fdb0abd81cac
tannmayygupta <guptatb_1@rknec.edu>
2025-03-08 19:20:16 +0530

    learned forEach(callback)

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-function sum(callback , x , y ){
-    let res = x + y ;
-    callback(res);
+// forEach() = method used to iterate over the lement in array and apply a specific callback function ;
+
+
+let arr = [1,2,3,4,5];
+
+arr.forEach(display); // here forEach calback fuc have parameter or attribute element , 
+
+arr.forEach(double); // here forEach calback fuc have parameter or attribute element , 
+
+arr.forEach(triple); // here forEach calback fuc have parameter or attribute element , 
+
+// arr.forEach(display); // here forEach calback fuc have parameter or attribute element , 
+
+// function double(element , index , array){
+//     array[index] = element * 2 ;
+// }
+
+function triple(element , index , array){
+    // array[index] = element * 3 ;
+    console.log(element * 3);
+
 }
 
-function displaypage(res){
-    document.getElementById("myH1").innerHTML = res;
+function display(element){
+    console.log(element);
 }
-
-sum(displaypage,5,5);
